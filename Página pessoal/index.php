2e199ccf14168a69ec88ca4f2fcca0e57c520118
Gabriel Borges <gabriel.bgs00@gmail.com>
2020-04-19 22:57:20 -0400

    pag pessoa php

--- Página pessoal/index.php
+++ Página pessoal/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MEU_NOME - Perfil</title>
+  <title>Gabriel Borges Gutierrez - Perfil</title>
   <link rel="stylesheet" href="estilos.css">
 </head>
 
@@ -12,15 +12,16 @@
   <div class="conteudo">
     <div class="foto"><img src="foto.png" alt=""></div>
     <div class="apresentacao">
-      <span class="nome">MEU_NOME</span>
-      <span class="curso">MEU_CURSO (NIVEL)</span>
-      <span class="email">MEU_EMAIL</span>
+      <span class="nome">Gabriel Borges Gutierrez</span>
+      <span class="curso">Engenharia de software (incompleto)</span>
+      <span class="email">gabriel.bgs00@gmail.com</span>
     </div>
   </div>
   <footer>
-    <!-- Insira aqui o texto abaixo substituindo pelo conteudo correto -->
-    <!-- Texto: Página gerada em dia/mes/ano as hora:minuto:segundo -->
-    <!-- Você deverá usar PHP e configurar para o timezone de Campo Grande/MS -->
+   <spam>
+   <?date_default_timezone_set('America/Campo_Grande') ?>
+   <?php echo'Página gerada em '. date('d/m/Y \à\s H:i:s')?>
+   </spam>
   </footer>
 </body>
 
